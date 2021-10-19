e6a7d2b7f8301fcc7643d69f530c6569cc302ecc
ferminmedina1 <ferminmedina2002@gmail.com>
2021-10-18 21:33:58 -0300

    add points and improvements

--- js/game.js
+++ js/game.js
@@ -3,24 +3,62 @@
 document.addEventListener('DOMContentLoaded', loadGame);
 
 function loadGame () {
+    let puntos = 0;
+    var keyDown = false;
+    let temporizador = 30000; //en ms.
     let background = document.querySelector(".game");
     let player = document.querySelector(".player");
 
     background.style.webkitAnimationPlayState = "running";
     player.classList.add("running");  
 
-    document.addEventListener("keydown", function (e){
+    window.addEventListener("keydown", function (e){
         if(e.code == "KeyW" || e.code == "ArrowUp" || e.key == " "){
-            console.log("Se apreto el boton hacia arriba.");
-            player.classList.add("jump");
+            keyDown = true;
+        }  
+    })
 
+    window.addEventListener("keyup", function resetAnimation(e){
+        if(e.code == "KeyW" || e.code == "ArrowUp" || e.key == " "){
+            keyDown = false;
+        } 
+    });
+    
+    let start, previousTimeStamp;
+    function step(timeStamp){
+        if(start === undefined){
+            start = timeStamp
+        }
+        const elapsed = timeStamp - start;
+
+        if(previousTimeStamp !== timeStamp){
+            if(keyDown){
+                console.log("Se apreto el boton hacia arriba.");
+                player.classList.add("jump");
+            }
+            else{
+                console.log("se dejo de apretar el boton hacia arriba.");
+                player.className = "player running";
+            }
         }
 
-        document.addEventListener("keyup", function resetAnimation(e){
-            console.log("se dejo de apretar el boton hacia arriba.");
-            player.className = "player running";
-        });
+        if(elapsed < temporizador){
+            previousTimeStamp = timeStamp
+            window.requestAnimationFrame(step);
+        }else{
+            document.querySelector(".points").innerHTML = "Se termino el tiempo... Puntaje: " + puntos;
+            player.className = "player";
+            background.style.webkitAnimationPlayState = "paused";
+            console.log("fin!");
+        }
+    }
 
-    })
-    
+    window.requestAnimationFrame(step);
+
+    setInterval(function(){
+        if(background.style.webkitAnimationPlayState != "paused"){
+            document.querySelector(".points").innerHTML = puntos;
+            puntos++;
+        }
+        },500);
 }
